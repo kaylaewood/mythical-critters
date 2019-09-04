9ca6de569c6e8bc2f95408444c5d565663072959
Kayla Wood <kaylaewood@gmail.com>
2019-09-04 08:47:15 -0600

    Complete direwolf

---
+++ exercises/direwolf.js
@@ -0,0 +1,27 @@
+class Direwolf {
+  constructor(name, home, size) {
+    this.name = name;
+    this.home = home || 'Beyond the Wall';
+    this.size = size || 'Massive';
+    this.starksToProtect = [];
+    this.huntsWhiteWalkers = true;
+  }
+
+  protect(stark) {
+    if (this.home === stark.location && this.starksToProtect.length < 2) {
+      this.starksToProtect.push(stark);
+      stark.safe = true;
+      this.huntsWhiteWalkers = false;
+    } else {
+      stark.safe = false;
+    }
+  }
+
+  leave(stark) {
+    this.starksToProtect.pop();
+    stark.safe = false;
+  }
+
+};
+
+module.exports = Direwolf;
